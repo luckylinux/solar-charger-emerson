5adb211a03d8c8e7a57de2b4978e12716fbf0319
luckylinux <luckylinux>
2024-02-27 20:22:12 +0100

    Modified Dockerfiles and docker-entrypoint.sh folder structure, so that the app is now installed in /opt/solar-charger-emerson and the Python code is installed in /opt/solar-charger-emerson/app on the fly via git directly from master branch

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
+# Change to app folder
+cd /opt/solar-charger-emerson
+
 # Setup git repository source
-git init .
-cd .
-git remote add -f origin https://github.com/luckylinux/solar-charger-emerson.git
+git init --initial-branch=main
 git config core.sparseCheckout true
 git config init.defaultBranch main
+git remote add -f origin https://github.com/luckylinux/solar-charger-emerson.git
+git branch --set-upstream-to=origin/main main
 
 # Reset sparse checkout file list
-rm -f .git/info/sparse-checkout
+#rm -f .git/info/sparse-checkout
 
 # Select which folders to checkout
 echo "app/" >> .git/info/sparse-checkout
 echo "app/lib/" >> .git/info/sparse-checkout
 
 # Pull latest version
-git pull --depth=1
+git pull --no-commit origin main --ff-only --depth=1
 
-while true
-do
-    sleep 2
-done
+# Infinite loop to debug
+#while true
+#do
+#    sleep 5
+#done
 
 # Launch App
-python -u app.py
+python -u app/app.py
